fbc3b6afede15b0a0b49014eed40ca7299ec9277
ronjian <835293711@qq.com>
2020-07-17 15:14:05 +0800

    update yolov5s model
    Signed-off-by: ronjian <835293711@qq.com>

--- examples/pytorch/yolov5s/convert.py
+++ examples/pytorch/yolov5s/convert.py
@@ -9,12 +9,12 @@ if __name__ == '__main__':
 
         # pre-process config
         print('--> config model')
-        rknn.config(channel_mean_value='123.675 116.28 103.53 58.395', reorder_channel='0 1 2', target_platform='rv1126')
+        rknn.config(channel_mean_value='123.675 116.28 103.53 58.395', reorder_channel='0 1 2', target_platform='rv1126', batch_size = 200)
         print('done')
 
         # Load pytorch model
         print('--> Loading model')
-        ret = rknn.load_pytorch(model='/workspace/yolov5/weights/yolov5s_jit.pt', input_size_list=input_size_list)
+        ret = rknn.load_pytorch(model='/workspace/yolov5/weights/best_yolov5s_robo_inconv_jit.pt', input_size_list=input_size_list)
         if ret != 0:
             print('Load pytorch model failed!')
             exit(ret)
@@ -22,8 +22,11 @@ if __name__ == '__main__':
 
         # Build model
         print('--> Building model')
-        # ret = rknn.build(do_quantization=False, pre_compile=True)
-        ret = rknn.build(do_quantization=True, dataset='dataset.txt', pre_compile=True, rknn_batch_size=1)
+        # ret = rknn.build(do_quantization=True, dataset='dataset.txt', pre_compile=True, rknn_batch_size=1)
+        ret = rknn.build(do_quantization=True
+                        , dataset='/workspace/rockchip/RV1126_RV1109/rv1126_rv1109/external/rknn-toolkit/examples/pytorch/centernet/baiguang-val-dataset.txt'
+                        , pre_compile=True
+                        , rknn_batch_size=1)
         if ret != 0:
             print('Build pytorch failed!')
             exit(ret)
@@ -31,8 +34,13 @@ if __name__ == '__main__':
 
         # Export rknn model
         print('--> Export RKNN model')
-        ret = rknn.export_rknn('./yolov5s.rknn')
+        ret = rknn.export_rknn('./best_yolov5s_robo_inconv.rknn')
         if ret != 0:
             print('Export failed!')
             exit(ret)
+        print('done')
+
+        print('--> Analysis')
+        rknn.accuracy_analysis('/workspace/rockchip/RV1126_RV1109/rv1126_rv1109/external/rknn-toolkit/examples/pytorch/centernet/baiguang_anaysis.txt'
+                                , output_dir='./snapshot', calc_qnt_error=True)
         print('done')
