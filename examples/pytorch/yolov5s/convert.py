7d1a64099e0755ac6b2223639a3391d8ecbd7bc1
ronjian <835293711@qq.com>
2020-07-24 14:47:46 +0800

    sync
    Signed-off-by: ronjian <835293711@qq.com>

--- examples/pytorch/yolov5s/convert.py
+++ examples/pytorch/yolov5s/convert.py
@@ -2,27 +2,34 @@
 from rknn.api import RKNN
 import torch
 FLOAT = False
+PC=False
 if __name__ == '__main__':
         # Create RKNN object
         rknn = RKNN()
-        input_size_list = [[3,640,640]]
+        input_size_list = [[3,512,672]]
 
         # pre-process config
         print('--> config model')
-        if FLOAT:
-            rknn.config(channel_mean_value='0. 0. 0. 255.', reorder_channel='0 1 2', target_platform='rv1126')
-        else:
+        if PC:
             rknn.config(channel_mean_value='0. 0. 0. 255.'
                         , reorder_channel='0 1 2'
-                        , target_platform='rv1126'
                         , batch_size = 10
-                        , quantized_dtype='dynamic_fixed_point-16'
-                        )
+                        , quantized_dtype='dynamic_fixed_point-16')
+        else:
+            if FLOAT:
+                rknn.config(channel_mean_value='0. 0. 0. 255.', reorder_channel='0 1 2', target_platform='rv1126')
+            else:
+                rknn.config(channel_mean_value='0. 0. 0. 255.'
+                            , reorder_channel='0 1 2'
+                            , target_platform='rv1126'
+                            , batch_size = 10
+                            # , quantized_dtype='dynamic_fixed_point-16'
+                            )
         print('done')
 
         # Load pytorch model
         print('--> Loading model')
-        ret = rknn.load_pytorch(model='/workspace/yolov5/weights/best_yolov5s_robo_inconv_jit.pt', input_size_list=input_size_list)
+        ret = rknn.load_pytorch(model='/workspace/yolov5/weights/l', input_size_list=input_size_list)
         if ret != 0:
             print('Load pytorch model failed!')
             exit(ret)
@@ -30,14 +37,20 @@ if __name__ == '__main__':
 
         # Build model
         print('--> Building model')
-        if FLOAT:
-            ret = rknn.build(do_quantization=False, pre_compile=True)
-        else:
+        if PC:
             ret = rknn.build(do_quantization=True
-                        , dataset='/workspace/rockchip/RV1126_RV1109/rv1126_rv1109/external/rknn-toolkit/examples/pytorch/centernet/baiguang-val-dataset.txt'
-                        # , dataset='./dataset.txt'
-                        , pre_compile=True
-                        , rknn_batch_size=1)
+                            , dataset='/workspace/rockchip/RV1126_RV1109/rv1126_rv1109/external/rknn-toolkit/examples/pytorch/centernet/baiguang-val-dataset.txt'
+                            # , dataset='./dataset.txt'
+                            , rknn_batch_size=1)
+        else:
+            if FLOAT:
+                ret = rknn.build(do_quantization=False, pre_compile=True)
+            else:
+                ret = rknn.build(do_quantization=True
+                            , dataset='/workspace/rockchip/RV1126_RV1109/rv1126_rv1109/external/rknn-toolkit/examples/pytorch/centernet/baiguang-val-dataset.txt'
+                            # , dataset='./dataset.txt'
+                            , pre_compile=True
+                            , rknn_batch_size=1)
         if ret != 0:
             print('Build pytorch failed!')
             exit(ret)
@@ -45,10 +58,13 @@ if __name__ == '__main__':
 
         # Export rknn model
         print('--> Export RKNN model')
-        if FLOAT:
-            ret = rknn.export_rknn('./best_yolov5s_robo_inconv.rknn-float-precompiled')
+        if PC:
+            ret = rknn.export_rknn('./best_yolov5s_robo_inconv.rknn-fp16-pc')
         else:
-            ret = rknn.export_rknn('./best_yolov5s_robo_inconv.rknn-fp16-all-10')
+            if FLOAT:
+                ret = rknn.export_rknn('./best_yolov5s_robo_inconv.rknn-float-precompiled')
+            else:
+                ret = rknn.export_rknn('./best_yolov5s_robo_inconv.rknn-int8-all-10')
         if ret != 0:
             print('Export failed!')
             exit(ret)
--- examples/pytorch/yolov5s/inference.py
+++ examples/pytorch/yolov5s/inference.py
@@ -11,7 +11,42 @@ import os
 # pad_bottom = 640 - image_height - pad_top
 # val_dir = '/workspace/centernet/data/baiguang/images/val/'
 # txt_dir = './assets/baiguang_text/'
-
+classnames = ["__background__", "wire", "pet feces", "shoe", "bar stool a", "fan", "power strip", "dock(ruby)",
+                 "dock(rubys+tanosv)", "bar stool b", "scale", "clothing item", "cleaning robot",
+                 "fan b", "door mark a", "door mark b", "wheel", "door mark c", "flat base", "whole fan",
+                 "whole fan b", "whole bar stool a", "whole bar stool b", "fake poop a", "fake poop b", "dust pan",
+                 "folding chair", "laundry basket", "handheld cleaner", "sock"]
+colors = [None
+, (0  , 255, 0)
+,(255, 0  , 0)
+,(0  , 0  , 255)
+,(255, 0  , 255)
+,(0  , 255, 255)
+,(255, 255, 0)
+,(0  , 127, 0)
+,(0  , 0, 127)
+,(127, 0, 0)
+,(127, 0  , 255)
+,(0  , 127, 255)
+,(100, 50 , 200)
+,(0  , 127, 255)
+,(127, 255, 255)
+,(255, 127, 255)
+,(255, 255, 127)
+,(127, 127, 255)
+,(127, 255, 127)
+,(255, 127, 127)
+,(127, 100, 127)
+,(100, 127, 127)
+,(127, 127, 100)
+,(100, 127, 50) 
+,(50 , 200, 127)
+,(255, 100, 127)
+,(100, 255, 127)
+,(255, 50 , 127)
+,(200, 255, 127)
+,(200, 100, 127)
+]
 class_cnt = 29
 an_vec = class_cnt + 1 + 4
 an_c = an_vec * 3
@@ -44,71 +79,80 @@ def parse_det(an, an_h, an_w, an_s, an_wh):
                     # if obj_conf and obj_conf > 0.3 and val > 0.5:
                     if obj_conf > 0.1:
                         score = obj_conf * val
-                        print(an_h, an_w, h, w, c % an_vec, c // an_vec, c_x, c_y, b_w, b_h, obj_conf, val, score)
-                        res.append((c_x, c_y, b_w, b_h))
+                        # print(an_h, an_w, h, w, c % an_vec, c // an_vec, c_x, c_y, b_w, b_h, obj_conf, val, score)
+                        res.append((c_x, c_y, b_w, b_h, score, int(c % an_vec-4)))
     return res   
 
 if __name__ == "__main__":
     rknn = RKNN()
     rknn.config(channel_mean_value='0. 0. 0. 255.', reorder_channel='0 1 2')
-    ret = rknn.load_rknn('./best_yolov5s_robo_inconv.rknn-float')
+    ret = rknn.load_rknn('./best_yolov5s_robo_inconv.rknn-quant-pc')
     assert ret == 0
+    val_dir = '/workspace/centernet/data/baiguang/images/val/'
+    for imname in  os.listdir(val_dir):
+        if imname.endswith('.jpeg') and imname == "StereoVision_L_1466362_46_0_1_1401_D_FakePoop_1492_1890.jpeg":
+            print(imname)
+            origin_img = cv2.imread(val_dir + imname)
+            origin_height, origin_width, _ = origin_img.shape
+            image_height = int(image_width / origin_width * origin_height)
+            pad_top = (image_width - image_height) // 2
+            pad_bottom = image_width - image_height - pad_top
+            input_img = cv2.resize(origin_img, (image_width, image_height))
+            input_img = cv2.copyMakeBorder(input_img
+                                                , top = pad_top
+                                                , bottom = pad_bottom
+                                                , left=0
+                                                , right=0
+                                                , borderType=cv2.BORDER_CONSTANT
+                                                , value=(114, 114, 114))  # add border
+            assert input_img.shape == (image_width, image_width, 3)
+            # cv2.imwrite('./assets/input.jpg', input_img)
+            # print(input_img.shape)
+            ret = rknn.init_runtime()
+            assert ret == 0
 
-    origin_img = cv2.imread('/workspace/centernet/data/baiguang/images/val/StereoVision_L_990964_10_0_0_5026_D_FakePoop_719_-149.jpeg')
-    origin_height, origin_width, _ = origin_img.shape
-    image_height = int(image_width / origin_width * origin_height)
-    pad_top = (image_width - image_height) // 2
-    pad_bottom = image_width - image_height - pad_top
-    input_img = cv2.resize(origin_img, (image_width, image_height))
-    input_img = cv2.copyMakeBorder(input_img
-                                        , top = pad_top
-                                        , bottom = pad_bottom
-                                        , left=0
-                                        , right=0
-                                        , borderType=cv2.BORDER_CONSTANT
-                                        , value=(114, 114, 114))  # add border
-    assert input_img.shape == (image_width, image_width, 3)
-    # cv2.imwrite('./assets/input.jpg', input_img)
-    # print(input_img.shape)
-    ret = rknn.init_runtime()
-    assert ret == 0
+            outputs = rknn.inference(inputs=[input_img])
+            # print(len(outputs), outputs[0].shape, outputs[1].shape, outputs[2].shape)
+            # np.save('assets/output0.npy', outputs[0])
+            # np.save('assets/output1.npy', outputs[1])
+            # np.save('assets/output2.npy', outputs[2])
 
-    outputs = rknn.inference(inputs=[input_img])
-    # print(len(outputs), outputs[0].shape, outputs[1].shape, outputs[2].shape)
-    # np.save('assets/output0.npy', outputs[0])
-    # np.save('assets/output1.npy', outputs[1])
-    # np.save('assets/output2.npy', outputs[2])
+            # img = cv2.imread('/workspace/centernet/data/baiguang/images/val/StereoVision_L_990964_10_0_0_5026_D_FakePoop_719_-149.jpeg')
+            # img = cv2.imread('./assets/input.jpg')
+            R = []
+            R += parse_det(outputs[2].flatten().tolist()
+                    , 20
+                    , 20
+                    , 32
+                    , [[214,99], [287,176], [376,365]])
 
-    # img = cv2.imread('/workspace/centernet/data/baiguang/images/val/StereoVision_L_990964_10_0_0_5026_D_FakePoop_719_-149.jpeg')
-    # img = cv2.imread('./assets/input.jpg')
-    R = []
-    R += parse_det(outputs[2].flatten().tolist()
-            , 20
-            , 20
-            , 32
-            , [[214,99], [287,176], [376,365]])
+            R += parse_det(outputs[1].flatten().tolist()
+                    , 40
+                    , 40
+                    , 16
+                    , [[94,219], [120,86], [173,337]])
 
-    R += parse_det(outputs[1].flatten().tolist()
-            , 40
-            , 40
-            , 16
-            , [[94,219], [120,86], [173,337]])
+            R += parse_det(outputs[0].flatten().tolist()
+                    , 80
+                    , 80
+                    , 8
+                    , [[28,31], [53,73], [91,39]] )
 
-    R += parse_det(outputs[0].flatten().tolist()
-            , 80
-            , 80
-            , 8
-            , [[28,31], [53,73], [91,39]] )
+            with open('./assets/txt/' + imname.replace('jpeg', 'txt'), 'w') as wf:
+                for box in R:
+                    c_x, c_y, b_w, b_h, score, classid = box
+                    if score > 0.1:
+                        # score = round(score,4)
+                        x0 = int(clip((c_x - b_w / 2.) / image_width) * origin_width)
+                        y0 = int(clip((c_y - b_h / 2. - pad_top) / image_height) * origin_height)
+                        x1 = int(clip((c_x + b_w / 2.) / image_width) * origin_width)
+                        y1 = int(clip((c_y + b_h / 2. - pad_top) / image_height) * origin_height)
+                        origin_img = cv2.rectangle(origin_img, (x0, y0), (x1, y1), colors[classid], 2)
+                        classname = classnames[classid].replace(' ','-')
+                        # print(x0, y0, x1, y1, score, classid, classname)
+                        wf.write("{} {} {} {} {} {}\n".format(classname, score, x0, y0, x1, y1))
 
-    for box in R:
-        c_x, c_y, b_w, b_h = box
-        x0 = int(clip((c_x - b_w / 2.) / image_width) * origin_width)
-        y0 = int(clip((c_y - b_h / 2. - pad_top) / image_height) * origin_height)
-        x1 = int(clip((c_x + b_w / 2.) / image_width) * origin_width)
-        y1 = int(clip((c_y + b_h / 2. - pad_top) / image_height) * origin_height)
-        origin_img = cv2.rectangle(origin_img, (x0, y0), (x1, y1), (255, 0,0), 2)
-        
-    cv2.imwrite('./assets/StereoVision_L_990964_10_0_0_5026_D_FakePoop_719_-149.jpeg', origin_img)
+            cv2.imwrite('./assets/vis/'+imname, origin_img)
 
     rknn.release()
 
